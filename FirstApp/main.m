21a24255c571820baf7a43a8c9e469c0b6d0537f
dicristomanuel <manuel.dicristo@raise.com>
2016-09-04 14:48:20 -0500

    Initial Commit

---
+++ FirstApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Manuel Di Cristo on 9/4/16.
+//  Copyright © 2016 manueldicristo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
