5529e9b20341162d394b9d97bf453a6f3f1f1491
dicristomanuel <manuel.dicristo@raise.com>
2016-09-05 17:28:02 -0500

    [app] main function body

--- FirstApp/main.m
+++ FirstApp/main.m
@@ -7,11 +7,36 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Employee.h"
+
+bool logString(int minutesInAYear) {
+    if (minutesInAYear == 525600) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int main(int argc, const char * argv[]) {
+
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+        int minutesInAYear = days * hours * minutes;
+
+        NSString *message = @"foobar";
+        NSDate *today = [NSDate date];
+        
+        Employee *fred = [[Employee alloc] init];
+        
+        [fred someMethod];
+        
+        if (logString(minutesInAYear)) {
+            NSLog(@"There are %i minutes in a year. Today's date is %@ - also %@", minutesInAYear, today, message);
+        } else {
+            NSLog(@"%i is the wrong number", minutesInAYear);
+        }
     }
     return 0;
 }
---
+++ NSObject+Employee.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Employee.h
+//  FirstApp
+//
+//  Created by Manuel Di Cristo on 9/5/16.
+//  Copyright © 2016 manueldicristo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Employee)
+
+@end
